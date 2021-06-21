0a6451b4c423e113ffb05884e685406011402cb7
Paulos Thomas <paulost72@gmail.com>
2021-06-21 08:09:31 -0700

    added json parsing and hash manipulation

--- nyc_api.rb
+++ nyc_api.rb
@@ -21,5 +21,7 @@ require 'net/http'
 
 end
 
- programs = GetPrograms.new.get_programs
- puts programs
+#  programs = GetPrograms.new.get_programs
+#  puts programs
+programs = GetPrograms.new
+puts programs.program_school.uniq
